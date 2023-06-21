aada0ddef02e5659b463036c49ee23e88aa79e8b
Xamryx <gbeminiyiayomide37@gmail.com>
2023-06-20 21:18:23 -0700

    llll

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
  /**
  *print_sign - Determines if the input number
  *greater, iqual or less than zero
  *
  *@n: The input number as an integer.
  *
- Return: 1 is greater than zerpo. 0  zero.
+ Return: 1 is greater than zero. 0 zero.
  * -1 is less than zero
  */
 int print_sign(int n)
