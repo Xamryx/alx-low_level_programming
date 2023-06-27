429be208d41fd044a20855649d5ef1f088e0fd33
Xamryx <gbeminiyiayomide37@gmail.com>
2023-06-27 13:16:21 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *rst_to_98 - a function that takes a pointer to an int as parameter
+ *and updates the value it point to 98
+ *@n: input
+ *Retunn: n
+ */
+void reset_to_98(int*n)
+{
+*n=98;
+}
